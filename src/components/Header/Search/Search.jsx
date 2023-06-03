e75d008233c6736a09e3a6be251244910ced1662
@kushagra24 <kushagra@incentiv.finance>
2023-06-03 11:21:22 +0530

    Search result Ui done

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
 import { useNavigate } from "react-router-dom";
-
+import prod from "../../../assets/products/earbuds-prod-1.webp";
 const Search = ({ setShowSearch }) => {
   return (
     <>
@@ -11,6 +11,19 @@ const Search = ({ setShowSearch }) => {
           <input type='text' autoFocus placeholder='Search for products' />
           <div className='close-btn'>
             <MdClose onClick={() => setShowSearch(!setShowSearch)} />
+          </div>
+        </div>
+        <div className='search-result-content'>
+          <div className='search-results'>
+            <div className='search-result-item'>
+              <div className='image-container'>
+                <img src={prod} alt='' />
+              </div>
+              <div className='prod-details'>
+                <span className='name'>Product Name</span>
+                <span className='desc'>Product Description</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
